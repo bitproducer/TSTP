9b7e83bc75e331af77e4a19e0a99f4290e866913
bitproducer <benjamin.krebs@gmail.com>
2020-12-11 12:41:42 -0800

    my first commit.

--- test.py
+++ test.py
@@ -6,5 +6,15 @@ for n in range(len(name)):          #for each letter in the input
     print(name[n])                  #print the letter
 """
 
+"""
 s = 'It was a bright cold day in April, and the clocks were striking thirdteen.'
 print(s[:s.index(',')])
+"""
+
+x = 100
+
+if x < 10:
+	print ('x is smaller then 10')
+
+else:
+	print ('x is bigger then 10')
